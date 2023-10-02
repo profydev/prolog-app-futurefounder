5975aabd6e56ed2cae24523673a25dc2454cffce
Jesse <47643039+futurefounder@users.noreply.github.com>
2023-10-02 10:58:11 +0200

    Projects error screen (#10)
    * feat: Add conditional rotation to sidebar collapse arrow
    
    - Implement 180-degree rotation to the arrow icon when the sidebar is collapsed
    - Add smooth 0.3s transition animation for the rotation
    
    * Fix: Update project card line thickness
    
    The separator line in project cards matches the design with this commit.
    
    * Fix: Update text color of page info to match design
    
    * fix: Correct navigation behavior of Support button
    
    * fix: Correct navigation behavior of Support button
    
    * fix: resolve linting issues
    
    * fix: resolve additional linting issues
    
    * fix: resolve further linting issues
    
    * fix: resolve further linting issues with prettier
    
    * fix: Correct logo display based on screen size
    
    * Fix issue-list: Correctly display Events and Users
    
    * git commit -m "feat: Add correct badge color, text rendering, and Cypress tests"
    
    * fix: resolve linting issues
    
    * fix type issues
    
    * fix: linting issue
    
    * feat: add footer component to dashboard pages
    
    * fix linting error
    
    * fix linting issues: styles
    
    * fix linting issues
    
    * feat: Add loading screen, fix sidebar link margins
    
    - Implement loading screen for project list
    - Adjust margins for link elements in the sidebar for better spacing
    
    * fix linting issues
    
    * feat (project-list): add error screen for better UX
    
    * fix linting error : code
    
    * fix linting error
    
    * fix cypress errors

--- cypress/e2e/project-list.cy.ts
+++ cypress/e2e/project-list.cy.ts
@@ -77,15 +77,46 @@ describe("Project List", () => {
           cy.wrap($el).contains(renderedStatuses[index]);
         });
     });
+
+    it("color of the status badge matches the project status", () => {
+      const statusColors = {
+        warning: "rgb(255, 250, 235)",
+        info: "rgb(236, 253, 243)",
+        critical: "rgb(254, 243, 242)",
+      };
+
+      cy.get("main")
+        .find("li")
+        .each(($el, index) => {
+          const expectedStatus = mockProjects[index].status;
+          if (expectedStatus in statusColors) {
+            cy.wrap($el)
+              .find(".badge_container__FVLnl")
+              .should(
+                "have.css",
+                "background-color",
+                statusColors[expectedStatus],
+              );
+          }
+        });
+    });
+
+    it("text writes 'Critical' for the error status and 'Stable' for the info status", () => {
+      cy.get("main")
+        .find("li")
+        .each(($el, index) => {
+          cy.wrap($el).contains(renderedStatuses[index]);
+        });
+    });
   });
 
   context("loading screen", () => {
     before(() => {
-      skipWait = true; // Set the flag to skip waiting
+      skipWait = true;
     });
 
     after(() => {
-      skipWait = false; // Reset the flag after the test
+      skipWait = false;
     });
     it("should display the loading screen while fetching data", () => {
       // Don't wait for the API to resolve yet, check for spinner immediately
@@ -97,6 +128,45 @@ describe("Project List", () => {
 
       // Check if the loading spinner is no longer displayed
       cy.get(".spinner_spinnerCircle__UXPFN").should("not.exist");
+    });
+  });
+
+  context("error handling", () => {
+    beforeEach(() => {
+      // Mock a failed request
+      cy.intercept("GET", "https://prolog-api.profy.dev/project", {
+        statusCode: 500,
+        body: {
+          error: "Internal Server Error",
+        },
+      }).as("getProjectsFailed");
+
+      cy.visit("http://localhost:3000/dashboard");
+    });
+
+    it("should display an error message when the request fails", () => {
+      // Wait for the failed request to complete
+      cy.wait("@getProjectsFailed");
+      cy.wait(5000);
+      // Check if the error message is displayed
+      cy.get(".error_alertContainer__EPA7V")
+        .should("be.visible")
+        .and("contain", "There was a problem while loading the project data");
+    });
+    it("should reload data when 'Try again' button is clicked", () => {
+      // Wait for the failed request to complete
+      cy.wait("@getProjectsFailed");
+      cy.get(".spinner_spinnerCircle__UXPFN", { timeout: 60000 }).should(
+        "not.exist",
+      );
+
+      // Mock a successful request for the retry
+      cy.intercept("GET", "https://prolog-api.profy.dev/project", {
+        fixture: "projects.json",
+      }).as("getProjectsRetry");
+      cy.wait(5000);
+      cy.get("button").contains("Try again").click();
+      cy.wait("@getProjectsRetry").its("response.statusCode").should("eq", 200);
     });
   });
 });
--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -1,11 +1,13 @@
 import { ProjectCard } from "../project-card";
 import { useGetProjects } from "../../api/use-get-projects";
-import { Spinner } from "@features/ui";
-
+import { Spinner, ErrorComponent } from "@features/ui";
 import styles from "./project-list.module.scss";
 
 export function ProjectList() {
-  const { data, isLoading, isError, error } = useGetProjects();
+  const { data, isLoading, isError, error, refetch } = useGetProjects();
+  const reload = () => {
+    refetch();
+  };
 
   if (isLoading) {
     return <Spinner />;
@@ -13,7 +15,7 @@ export function ProjectList() {
 
   if (isError) {
     console.error(error);
-    return <div>Error: {error.message}</div>;
+    return <ErrorComponent onRetry={reload} />;
   }
 
   return (
