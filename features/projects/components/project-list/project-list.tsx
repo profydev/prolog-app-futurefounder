ca8610b105d05f9a3ffe0a9cd777267d52d1a0fe
futurefounder <jsk@posteo.de>
2023-10-02 10:25:33 +0200

    fix linting error : code

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -1,16 +1,15 @@
-import { useState, useEffect } from "react";
 import { ProjectCard } from "../project-card";
 import { useGetProjects } from "../../api/use-get-projects";
 import { Spinner, ErrorComponent } from "@features/ui";
 import styles from "./project-list.module.scss";
 
 export function ProjectList() {
-  const [trigger, setTrigger] = useState(0);
-  const { data, isLoading, isError, error } = useGetProjects();
-  const reload = () => setTrigger((prev) => prev + 1);
+  const { data, isLoading, isError, error, refetch } = useGetProjects();
+  const reload = () => {
+    refetch();
+  };
 
   if (isLoading) {
-    return <Spinner />;
     return <Spinner />;
   }
 
